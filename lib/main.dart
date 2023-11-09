0634cb8184427c5121c147676cde0916b9984246
heightuser <meriheightwidth0011@gmail.com>
2023-11-09 16:19:29 +0530

    image added

--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,7 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -21,6 +22,7 @@ class MyApp extends StatelessWidget {
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
+
   final String title;
 
   @override
@@ -39,20 +41,21 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+            Container(
+              height: MediaQuery.of(context).size.height *0.9,
+              width: MediaQuery.of(context).size.width *0.9,
+              decoration: const BoxDecoration(
+                color: Colors.red,
+                image: DecorationImage(
+                  image: AssetImage("assets/images/spider.png"),
+                  fit: BoxFit.cover,
+                ),
+              ),
+              child: const Text("Image"),
             ),
           ],
         ),
