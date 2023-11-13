73d825dfbbaa466003346fe708e998e9da0cae3a
royam <royamjain@gmail.com>
2023-11-13 20:28:21 +0530

    added mouse regin, text with stack

--- lib/components/custom_component.dart
+++ lib/components/custom_component.dart
@@ -24,42 +24,62 @@ class _CustomComponentState extends State<CustomComponent> {
 
   Widget _buildButton(
       String label, int imageIndex, double baseWidth, double baseHeight,
-      {double? top, double? left, double? right, double? bottom, double? rotate}) {
+      {double? top,
+      double? left,
+      double? right,
+      double? bottom,
+      double? rotate}) {
     return Positioned(
       top: top,
       left: left,
       right: right,
       bottom: bottom,
-      child: SizedBox(
-        height: 60,
-        width: baseWidth / 18,
-        child: ClipPolygon(
-          sides: 5,
-          borderRadius: baseWidth / 240,
-          rotate: rotate ?? 0.0,
-          boxShadows: [
-            PolygonBoxShadow(color: Colors.black, elevation: 1.0),
-            PolygonBoxShadow(color: Colors.grey, elevation: 5),
-          ],
-          child: ElevatedButton(
-            style: ButtonStyle(
-              backgroundColor: MaterialStateProperty.resolveWith<Color>(
-                    (Set<MaterialState> states) {
-                  if (states.contains(MaterialState.hovered)) {
-                    return Colors.purple.shade400;
-                  } else {
-                    return Colors.purple.shade100;
-                  }
-                },
-              ),
-            ),
-            onPressed: () {},
-            child: Text(label, style: const TextStyle(fontSize: 8),maxLines: 4,overflow: TextOverflow.clip,),
-            onHover: (bool isHovered) {
+      child: InkWell(
+        onTap: () {},
+        child: MouseRegion(
+          cursor: SystemMouseCursors.click,
+          onEnter: (_) {
               setState(() {
-                path = isHovered ? images[imageIndex] : basePath;
+                path = images[imageIndex];
               });
-            },
+          },
+          onExit: (_) {
+            setState(() {
+              path = basePath;
+            });
+          },
+          child: Container(
+            height: 65,
+            width: baseWidth / 16,
+            child: Stack(
+              children: [
+                ClipPolygon(
+                  sides: 5,
+                  borderRadius: baseWidth / 240,
+                  rotate: rotate ?? 0.0,
+                  boxShadows: [
+                    PolygonBoxShadow(color: Colors.black, elevation: 1.0),
+                    PolygonBoxShadow(color: Colors.grey, elevation: 2),
+                  ],
+                  child: Container(color: Colors.purple,),
+                ),
+                Positioned(
+                  top: 14,
+                  left: 15,
+                  child: SizedBox(
+                    width: baseWidth / 35,
+                    child: Text(
+                      label,
+                      textAlign: TextAlign.center,
+                      style:
+                      const TextStyle(fontSize: 7, fontWeight: FontWeight.bold, color: Colors.white),
+                      maxLines: 4,
+                      overflow: TextOverflow.fade,
+                    ),
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
@@ -97,9 +117,10 @@ class _CustomComponentState extends State<CustomComponent> {
             _buildButton("QR code for ordering", 1, baseWidth, baseHeight,
                 top: baseHeight / 35, left: baseWidth / 30, rotate: 10),
             _buildButton("Conference & Events", 1, baseWidth, baseHeight,
-                top: baseHeight / 16, left: baseWidth / 13,rotate: 335),
-            _buildButton("Girl", 1, baseWidth, baseHeight,
-                top: baseHeight / 4, right: baseWidth / 6),
+                top: baseHeight / 16, left: baseWidth / 13, rotate: 335),
+            _buildButton(
+                "Table Service (QR code at table)", 1, baseWidth, baseHeight,
+                top: baseHeight / 4, right: baseWidth / 6, rotate: 250),
             _buildButton("Night", 2, baseWidth, baseHeight)
                 .align(Alignment.center),
             _buildButton("Spiral", 3, baseWidth, baseHeight,
