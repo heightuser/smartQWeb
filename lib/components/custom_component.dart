40011c7e8ea5d92cb15923f8accc923a3a289413
Divyanshu Shukla <divyanshushuklazz10@gmail.com>
2023-11-13 17:31:07 +0530

    New images and code improvised

--- lib/components/custom_component.dart
+++ lib/components/custom_component.dart
@@ -11,13 +11,55 @@ class CustomComponent extends StatefulWidget {
 }
 
 class _CustomComponentState extends State<CustomComponent> {
-  // comment
-  String path = "assets/images/spider.png";
-  String basePath = "assets/images/spider.png";
+  List<String> images = [
+    "assets/images/image1.jpeg",
+    "assets/images/image2.jpeg",
+    "assets/images/image3.jpeg",
+    "assets/images/image4.jpeg",
+    "assets/images/image5.jpeg",
+    "assets/images/image6.jpeg"
+  ];
+  String path = "assets/images/Base.jpeg";
+  String basePath = "assets/images/Base.jpeg";
+
+  Widget _buildButton(
+      String label, int imageIndex, double baseWidth, double baseHeight,
+      {double? top, double? left, double? right, double? bottom}) {
+    return Positioned(
+      top: top,
+      left: left,
+      right: right,
+      bottom: bottom,
+      child: SizedBox(
+        height: 100,
+        width: baseWidth / 12,
+        child: ClipPolygon(
+          sides: 6,
+          borderRadius: baseWidth / 240,
+          rotate: 90.0,
+          boxShadows: [
+            PolygonBoxShadow(color: Colors.black, elevation: 1.0),
+            PolygonBoxShadow(color: Colors.grey, elevation: 5),
+          ],
+          child: ElevatedButton(
+            onPressed: () {},
+            child: Text(label),
+            onHover: (bool isHovered) {
+              setState(() {
+                path = isHovered ? images[imageIndex] : basePath;
+              });
+            },
+          ),
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     double baseHeight = min(MediaQuery.of(context).size.height * 0.9, 800);
     double baseWidth = min(MediaQuery.of(context).size.width * 0.9, 1200);
+
     return Center(
       child: Container(
         height: baseHeight,
@@ -25,7 +67,7 @@ class _CustomComponentState extends State<CustomComponent> {
         decoration: BoxDecoration(
           color: Colors.red,
           image: DecorationImage(
-            image: AssetImage(basePath),
+            image: AssetImage(images[0]),
             fit: BoxFit.cover,
           ),
         ),
@@ -41,165 +83,28 @@ class _CustomComponentState extends State<CustomComponent> {
                 ),
               ),
             ),
-            Positioned(
-                top: baseHeight/4,
-                left: baseWidth/6,
-                child: SizedBox(
-                  height: 100,
-                  width: baseWidth/12,
-                  child: ClipPolygon(
-                    sides: 6,
-                    borderRadius: 5,
-                    rotate: 90.0,
-                    boxShadows: [
-                      PolygonBoxShadow(color: Colors.black, elevation: 1.0),
-                      PolygonBoxShadow(color: Colors.grey, elevation: 5)
-                    ],
-                    child: ElevatedButton(
-                      onPressed: (){},
-                      child: const Text("Flower"),
-                      onHover: (bool isHovered) {
-                        if (isHovered) {
-                          setState(() {
-                            path = "assets/images/flower.jpg";
-                          });
-                        } else {
-                          setState(() {
-                            path = "assets/images/spider.png";
-                          });
-                        }
-                      },
-                    ),
-                  ),
-                )),
-            Positioned(
-                top: baseHeight/4,
-                right: baseWidth/6,
-                child: SizedBox(
-                  height: 100,
-                  width: baseWidth/12,
-                  child: ClipPolygon(
-                    sides: 6,
-                    borderRadius: 5,
-                    rotate: 90.0,
-                    boxShadows: [
-                      PolygonBoxShadow(color: Colors.black, elevation: 1.0),
-                      PolygonBoxShadow(color: Colors.grey, elevation: 5)
-                    ],
-                    child: ElevatedButton(
-                      onPressed: (){},
-                      child: const Text("Girl"),
-                      onHover: (bool isHovered) {
-                        if (isHovered) {
-                          setState(() {
-                            path = "assets/images/girl.JPG";
-                          });
-                        } else {
-                          setState(() {
-                            path = "assets/images/spider.png";
-                          });
-                        }
-                      },
-                    ),
-                  ),
-                )),
-            Positioned.fill(
-                child: Align(
-                  alignment: Alignment.center,
-                  child: SizedBox(
-                    height: 100,
-                    width: baseWidth/12,
-                    child: ClipPolygon(
-                      sides: 6,
-                      borderRadius: 5,
-                      rotate: 90.0,
-                      boxShadows: [
-                        PolygonBoxShadow(color: Colors.black, elevation: 1.0),
-                        PolygonBoxShadow(color: Colors.grey, elevation: 5)
-                      ],
-                      child: ElevatedButton(
-                        onPressed: (){},
-                        child: const Text("Night"),
-                        onHover: (bool isHovered) {
-                          if (isHovered) {
-                            setState(() {
-                              path = "assets/images/night.jpg";
-                            });
-                          } else {
-                            setState(() {
-                              path = "assets/images/spider.png";
-                            });
-                          }
-                        },
-                      ),
-                    ),
-                  ),
-                )),
-            Positioned(
-                bottom: baseHeight/4,
-                left: baseWidth/6,
-                child: SizedBox(
-                  height: 100,
-                  width: baseWidth/12,
-                  child: ClipPolygon(
-                    sides: 6,
-                    borderRadius: 5,
-                    rotate: 90.0,
-                    boxShadows: [
-                      PolygonBoxShadow(color: Colors.black, elevation: 1.0),
-                      PolygonBoxShadow(color: Colors.grey, elevation: 5)
-                    ],
-                    child: ElevatedButton(
-                      onPressed: (){},
-                      child: const Text("Spiral"),
-                      onHover: (bool isHovered) {
-                        if (isHovered) {
-                          setState(() {
-                            path = "assets/images/spiral.jpg";
-                          });
-                        } else {
-                          setState(() {
-                            path = "assets/images/spider.png";
-                          });
-                        }
-                      },
-                    ),
-                  ),
-                )),
-            Positioned(
-                bottom: baseHeight/4,
-                right: baseWidth/6,
-                child: SizedBox(
-                  height: 100,
-                  width: baseWidth/12,
-                  child: ClipPolygon(
-                    sides: 6,
-                    borderRadius: baseWidth/240,
-                    rotate: 90.0,
-                    boxShadows: [
-                      PolygonBoxShadow(color: Colors.black, elevation: 1.0),
-                      PolygonBoxShadow(color: Colors.grey, elevation: 5)
-                    ],
-                    child: ElevatedButton(
-                      onPressed: (){},
-                      child: const Text("Sky"),
-                      onHover: (bool isHovered) {
-                        if (isHovered) {
-                          setState(() {
-                            path = "assets/images/sky.jpg";
-                          });
-                        } else {
-                          setState(() {
-                            path = "assets/images/spider.png";
-                          });
-                        }
-                      },
-                    ),
-                  ),
-                )),
+            _buildButton("Flower", 0, baseWidth, baseHeight,
+                top: baseHeight / 4, left: baseWidth / 6),
+            _buildButton("Girl", 1, baseWidth, baseHeight,
+                top: baseHeight / 4, right: baseWidth / 6),
+            _buildButton("Night", 2, baseWidth, baseHeight)
+                .align(Alignment.center),
+            _buildButton("Spiral", 3, baseWidth, baseHeight,
+                bottom: baseHeight / 4, left: baseWidth / 6),
+            _buildButton("Sky", 4, baseWidth, baseHeight,
+                bottom: baseHeight / 4, right: baseWidth / 6),
           ],
         ),
       ),
+    );
+  }
+}
+
+extension PositionedExtension on Widget {
+  Widget align(Alignment alignment) {
+    return Align(
+      alignment: alignment,
+      child: this,
     );
   }
 }
